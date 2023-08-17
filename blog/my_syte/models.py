ee8f5b9e38f29ecc8b560b12904e59864f4b1ae9
mahdikalhor1 <mahdi.kalhor.81.21@gmail.com>
2023-08-17 15:23:44 +0330

    add required models

--- blog/my_syte/models.py
+++ blog/my_syte/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100)
+
+class Tag(models.Model):
+    content = models.CharField(max_length=50)
+
+
+class Post(models.Model):
+    post_name = models.CharField(max_length=50)
+    image = models.FilePathField(max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    tag = models.ManyToManyField(Tag)
+    data = models.DateField()
+    title = models.CharField(max_length=50)
+    export = models.TextField()
+    content = models.TextField()
