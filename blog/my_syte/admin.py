2049a739635f90fbdf3772ff4e8d332d50b21b4e
mahdikalhor1 <mahdi.kalhor.81.21@gmail.com>
2023-08-18 15:26:37 +0330

    change displaying of Post in admin

--- blog/my_syte/admin.py
+++ blog/my_syte/admin.py
@@ -2,8 +2,14 @@ from django.contrib import admin
 
 from .models import Post, Author, Tag
 
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields={'slug':('post_name',),}
+    list_filter = ['author', 'tag', 'date']
+    list_display = ['post_name', 'author', 'date']
+
 admin.site.register(Tag)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 admin.site.register(Author)
 
 
