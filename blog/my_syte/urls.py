6bc0b03f813d8b1b732956a045d68e5a04a877b6
mahdikalhor1 <mahdi.kalhor.81.21@gmail.com>
2023-08-25 11:47:27 +0330

    add author page

--- blog/my_syte/urls.py
+++ blog/my_syte/urls.py
@@ -3,6 +3,6 @@ from . import views
 urlpatterns = [
     path('', view= views.home_page , name = 'home-page'),
     path('posts/', view = views.posts, name= 'posts'),
-    path('posts/<slug:post_name>/', view= views.post_ditails, name='post-ditails'),
-    
+    path('posts/<slug:slug>/', view= views.post_ditails, name='post-ditails'),
+    path('author/<slug:slug>', view= views.author ,name='author')
 ]
--- blog/my_syte/views.py
+++ blog/my_syte/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from datetime import date
-from .models import Post
+from .models import Post, Author
 
 def get_date(post):
     return post.date
@@ -14,6 +14,16 @@ def posts(request):
     all_posts = Post.objects.all()
     return render(request, 'my_syte/all_posts.html', {"posts" : all_posts})
 
-def post_ditails(request, post_name):
-    post = Post.objects.get(slug=post_name)
+def post_ditails(request, slug):
+    post = get_object_or_404(Post, slug=slug)
     return render(request, 'my_syte/post_detail.html', {'post': post})
+
+def author(request, slug):
+    authors = Author.objects.all()
+
+    author = None
+    for a in authors: 
+        if a.slug() == slug:
+            author = a
+
+    return render(request, 'my_syte/author.html' ,{'author' : author})
