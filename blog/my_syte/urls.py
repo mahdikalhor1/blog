4c6f609a604d9a1aa292455c8f53aafaacbc4692
mahdikalhor1 <mahdi.kalhor.81.21@gmail.com>
2023-08-25 12:08:55 +0330

    replace view funtions to class views

--- blog/my_syte/urls.py
+++ blog/my_syte/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', view= views.home_page , name = 'home-page'),
-    path('posts/', view = views.posts, name= 'posts'),
-    path('posts/<slug:slug>/', view= views.post_ditails, name='post-ditails'),
-    path('author/<slug:slug>', view= views.author ,name='author')
+    path('', view= views.HomeView.as_view() , name = 'home-page'),
+    path('posts/', view = views.PostsView.as_view(), name= 'posts'),
+    path('posts/<slug:slug>/', view= views.PostDetail.as_view(), name='post-ditails'),
+    path('author/<slug:slug>', view= views.AuthorView.as_view() ,name='author')
 ]
--- blog/my_syte/views.py
+++ blog/my_syte/views.py
@@ -1,29 +1,69 @@
 from django.shortcuts import render, get_object_or_404
 from datetime import date
 from .models import Post, Author
+from django.views.generic import TemplateView, ListView
+from django.views.generic.detail import DetailView
 
 def get_date(post):
     return post.date
 
-def home_page(request):
-    all_posts = Post.objects.all()
-    latest_posts = sorted(all_posts, key=get_date)[-3:]
-    return render(request, 'my_syte/home_page.html', {'posts' : latest_posts})
+class HomeView(TemplateView):
+    template_name = 'my_syte/home_page.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        all_posts = Post.objects.all()
+        latest_posts = sorted(all_posts, key=get_date)[-3:]
 
-def posts(request):
-    all_posts = Post.objects.all()
-    return render(request, 'my_syte/all_posts.html', {"posts" : all_posts})
+        context['posts'] = latest_posts
+        
+        return context
 
-def post_ditails(request, slug):
-    post = get_object_or_404(Post, slug=slug)
-    return render(request, 'my_syte/post_detail.html', {'post': post})
+# def home_page(request):
+#     all_posts = Post.objects.all()
+#     latest_posts = sorted(all_posts, key=get_date)[-3:]
+#     return render(request, 'my_syte/home_page.html', {'posts' : latest_posts})
 
-def author(request, slug):
-    authors = Author.objects.all()
+class PostsView(ListView):
+    template_name = 'my_syte/all_posts.html'
+    model = Post
+    context_object_name = 'posts'
 
-    author = None
-    for a in authors: 
-        if a.slug() == slug:
-            author = a
+# def posts(request):
+#     all_posts = Post.objects.all()
+#     return render(request, 'my_syte/all_posts.html', {"posts" : all_posts})
 
-    return render(request, 'my_syte/author.html' ,{'author' : author})
+class PostDetail(DetailView):
+    template_name = 'my_syte/post_detail.html'
+    model = Post
+    context_object_name = 'post'
+
+# def post_ditails(request, slug):
+#     post = get_object_or_404(Post, slug=slug)
+#     return render(request, 'my_syte/post_detail.html', {'post': post})
+
+
+class AuthorView(TemplateView):
+    template_name='my_syte/author.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        authors = Author.objects.all()
+        
+        author = None
+        for a in authors: 
+            if a.slug() == kwargs['slug']:
+                author = a
+
+        context['author'] = author
+        return context
+
+
+# def author(request, slug):
+#     authors = Author.objects.all()
+#     author = None
+#     for a in authors: 
+#         if a.slug() == slug:
+#             author = a
+
+#     return render(request, 'my_syte/author.html' ,{'author' : author})
